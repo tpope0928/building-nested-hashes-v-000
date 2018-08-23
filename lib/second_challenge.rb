2f37d9d0d073f4a101d24a1202b7488e9b7e5d7a
Thomas Pope <tpope0928@gmail.com>
2018-08-23 23:24:48 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,3 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => []
-      }, 
-    :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
-      :hero_friends => []
-      }
-  }
-  
-end
+    
